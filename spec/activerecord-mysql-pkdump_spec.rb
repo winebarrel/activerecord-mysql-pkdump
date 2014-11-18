6c1d88dba427e1fa1b55e6d409d0079881b64b40
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-11-18 22:35:57 +0900

    Add spec

--- spec/activerecord-mysql-pkdump_spec.rb
+++ spec/activerecord-mysql-pkdump_spec.rb
@@ -1,5 +1,173 @@
 describe Activerecord::Mysql::Pkdump do
-  it do
-    expect(1).to eq 1
+  context 'when there is no table' do
+    it do
+      expect(dump).to eq <<-RUBY.strip
+ActiveRecord::Schema.define(version: 0) do
+
+end
+      RUBY
+    end
+  end
+
+  context 'when PK is `id`(int)' do
+    before do
+      create_tables(:id, 'int(11) primary key auto_increment')
+    end
+
+    it do
+      expect(dump).to eq <<-RUBY.strip
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "authors", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "books", force: true do |t|
+    t.string   "title",      null: false
+    t.integer  "author_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
+      RUBY
+    end
+  end
+
+  context 'when PK is `my_id`(int)' do
+    before do
+      create_tables(:my_id, 'int(11) primary key auto_increment')
+    end
+
+    it do
+      expect(dump).to eq <<-RUBY.strip
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "authors", primary_key: "my_id", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "books", primary_key: "my_id", force: true do |t|
+    t.string   "title",      null: false
+    t.integer  "author_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
+      RUBY
+    end
+  end
+
+  context 'when PK is `id`(bigint)' do
+    before do
+      create_tables(:id, 'bigint primary key auto_increment')
+    end
+
+    it do
+      expect(dump).to eq <<-RUBY.strip
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "authors", id: "bigint(20) PRIMARY KEY auto_increment", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "books", id: "bigint(20) PRIMARY KEY auto_increment", force: true do |t|
+    t.string   "title",      null: false
+    t.integer  "author_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
+      RUBY
+    end
+  end
+
+  context 'when PK is `my_id`(bigint)' do
+    before do
+      create_tables(:my_id, 'bigint primary key auto_increment')
+    end
+
+    it do
+      expect(dump).to eq <<-RUBY.strip
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "authors", primary_key: "my_id", id: "bigint(20) PRIMARY KEY auto_increment", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "books", primary_key: "my_id", id: "bigint(20) PRIMARY KEY auto_increment", force: true do |t|
+    t.string   "title",      null: false
+    t.integer  "author_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
+      RUBY
+    end
+  end
+
+  context 'when PK is `id`(varchar(255))' do
+    before do
+      create_tables(:id, 'varchar(255) primary key')
+    end
+
+    it do
+      expect(dump).to eq <<-RUBY.strip
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "authors", id: "varchar(255) PRIMARY KEY", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "books", id: "varchar(255) PRIMARY KEY", force: true do |t|
+    t.string   "title",      null: false
+    t.integer  "author_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
+      RUBY
+    end
+  end
+
+  context 'when PK is `my_id`(varchar(255))' do
+    before do
+      create_tables(:my_id, 'varchar(255) primary key')
+    end
+
+    it do
+      expect(dump).to eq <<-RUBY.strip
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "authors", primary_key: "my_id", id: "varchar(255) PRIMARY KEY", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "books", primary_key: "my_id", id: "varchar(255) PRIMARY KEY", force: true do |t|
+    t.string   "title",      null: false
+    t.integer  "author_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
+      RUBY
+    end
   end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,81 @@
+require 'active_record'
+require 'activerecord-mysql-pkdump'
+
+TEST_DATABASE = 'activerecord_mysql_pkdump_test'
+
+RSpec.configure do |config|
+  config.before(:all) do
+    ActiveRecord::Base.establish_connection(
+      adapter: 'mysql2',
+      database: TEST_DATABASE,
+    )
+  end
+
+  config.before(:each) do
+    clean_database
+  end
+end
+
+def mysql
+  client = nil
+  retval = nil
+
+  begin
+    client = Mysql2::Client.new(
+      host: 'localhost',
+      username: 'root',
+      database: TEST_DATABASE
+    )
+
+    retval = yield(client)
+  ensure
+    client.close if client
+  end
+
+  retval
+end
+
+def create_database(client)
+  client.query("CREATE DATABASE #{TEST_DATABASE}")
+end
+
+def drop_database(client)
+  client.query("DROP DATABASE IF EXISTS #{TEST_DATABASE}")
+end
+
+def clean_database
+  mysql do |client|
+    drop_database(client)
+    create_database(client)
+  end
+end
+
+def create_tables(pk_name, pk_type)
+  mysql do |client|
+    client.query(<<-SQL)
+      CREATE TABLE books (
+        `#{pk_name}` #{pk_type},
+        `title` varchar(255) NOT NULL,
+        `author_id` int(10) unsigned NOT NULL,
+        `created_at` datetime DEFAULT NULL,
+        `updated_at` datetime DEFAULT NULL
+      ) DEFAULT CHARSET=utf8
+    SQL
+
+    client.query(<<-SQL)
+      CREATE TABLE authors (
+        `#{pk_name}` #{pk_type},
+        `name` varchar(255) NOT NULL,
+        `created_at` datetime DEFAULT NULL,
+        `updated_at` datetime DEFAULT NULL
+      ) DEFAULT CHARSET=utf8;
+    SQL
+  end
+end
+
+def dump
+  conn = ActiveRecord::Base.connection
+  stream = StringIO.new
+  ActiveRecord::SchemaDumper.dump(conn, stream)
+  stream.string.gsub(/^#.*$/, '').strip
+end
